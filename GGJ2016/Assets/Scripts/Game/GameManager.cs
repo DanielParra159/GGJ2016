e9d3c8d68d377fc78e9363a9c98b6387087c358c
Daniel Parra <dani.parra.90@gmail.com>
2016-01-29 21:36:39 +0100

    + estructura

--- GGJ2016/Assets/Scripts/Game/GameManager.cs
+++ GGJ2016/Assets/Scripts/Game/GameManager.cs
@@ -1,30 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GameManager : MonoBehaviour {
-
-    public static GameManager instance = null;
-
-    void Awake()
-    {
-        //if (instance == null)
-        {
-            instance = this;
-            //    DontDestroyOnLoad(instance);
-        }
-        /* else if (instance != this)
-         {
-             Destroy(this.gameObject);
-         }*/
-    }
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
+﻿using UnityEngine;using System.Collections;public class GameManager : MonoBehaviour {    public static GameManager instance = null;    void Awake()    {        //if (instance == null)        {            instance = this;            //    DontDestroyOnLoad(instance);        }        /* else if (instance != this)         {             Destroy(this.gameObject);         }*/    }	// Use this for initialization	void Start () {		}		// Update is called once per frame	void Update () {		}}
