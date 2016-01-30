62a22a91d5408e7aab022de9da12ce3f2e985481
Daniel Parra <dani.parra.90@gmail.com>
2016-01-30 08:01:46 +0100

    + mecanica matar enemigos

--- GGJ2016/Assets/Scripts/Gameplay/KillEnemiesPuzzle.cs
+++ GGJ2016/Assets/Scripts/Gameplay/KillEnemiesPuzzle.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class KillEnemiesPuzzle : MonoBehaviour {
 
+
+    public Enemy[] enemies;
+    protected int currentEnemiesDead = 0;
+
+    public Door[] doorsToOpen;
+
+
 	// Use this for initialization
 	void Start () {
-	
+	    for (int i = 0; i < enemies.Length; ++i)
+        {
+            enemies[i].GetComponent<Life>().registerOnDead(onDead);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void onDead()
+    {
+        if (++currentEnemiesDead == enemies.Length)
+        {
+            //completado
+            for (int i = 0; i < doorsToOpen.Length; ++i)
+            {
+                doorsToOpen[i].open();
+            }
+            gameObject.SetActive(false);
+        }
+    }
 }
