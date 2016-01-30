548664a45bbedcc401c8093a7125c611ca76a4de
Jorge Castano <jorge.caster@gmail.com>
2016-01-30 14:12:09 +0100

    + Mapa vacío

--- GGJ2016/Assets/Scripts/Game/Mapa.cs
+++ GGJ2016/Assets/Scripts/Game/Mapa.cs
@@ -5,52 +5,13 @@ using System.IO;
 
 public class Mapa : MonoBehaviour {
 
-	public string fileName;
-	int[,] matrix;
-
 	// Use this for initialization
 	void Start () {
-		StreamReader theReader = new StreamReader("Assets/Resources/"+ fileName +".csv", Encoding.Default);
-		string line;
-		int matrix_line = 0;
-
-		using (theReader)
-		{
-
-			line = theReader.ReadLine();
-			string[] entries = line.Split('x');
-			matrix = new int[int.Parse(entries[1]), int.Parse(entries[0])];
-
-			// While there's lines left in the text file, do this:
-			do
-			{
-				line = theReader.ReadLine();
-
-				if (line != null)
-				{
-					entries = line.Split(',');
-					if (entries.Length > 0)
-					{
-						for (int i=0; i < entries.Length; i++)
-						{
-							matrix[matrix_line,i] = int.Parse(entries[i]);
-						}
-						matrix_line++;
-					}
-				}
-			}
-			while (line != null);
-			// Done reading, close the reader and return true to broadcast success    
-			theReader.Close();
-		}
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
-
-	public int[,] getMatrix() {
-		return matrix;
 	}
 }
