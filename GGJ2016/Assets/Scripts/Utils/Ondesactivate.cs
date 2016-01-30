229a8ba75a946041d9d4721287a183030558bcf6
Daniel Parra <dani.parra.90@gmail.com>
2016-01-30 14:28:52 +0100

    + desactivar disparos

---
+++ GGJ2016/Assets/Scripts/Utils/Ondesactivate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ondesactivate : MonoBehaviour {
+
+    public float time = 10.0f;
+    protected float currentTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnEnable()
+    {
+        currentTime = time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime -= Time.deltaTime;
+        if (currentTime<0.0f)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+}
