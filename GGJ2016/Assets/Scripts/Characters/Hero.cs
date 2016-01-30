9d3ee5a82426870051a5c39be02a1702d2619c05
Daniel Parra <dani.parra.90@gmail.com>
2016-01-30 11:10:05 +0100

    + importar sprite proyectil

--- GGJ2016/Assets/Scripts/Characters/Hero.cs
+++ GGJ2016/Assets/Scripts/Characters/Hero.cs
@@ -15,10 +15,17 @@ using System.Collections.Generic;[RequireComponent(typeof(Life))][RequireCom
         life.init();
     }	// Update is called once per frame	void Update () {        if (pausable.Check()) return;    }    void FixedUpdate()    {        if (pausable.Check()) return;
         timeBlocked -= Time.fixedDeltaTime;
-        if (timeBlocked > 0.0f) return;        movAxis = GamepadInput.GamePad.GetAxis(GamepadInput.GamePad.Axis.LeftStick, GamepadInput.GamePad.Index.One);        if (movAxis == Vector2.zero)        {            movAxis = GamepadInput.GamePad.GetAxis(GamepadInput.GamePad.Axis.KeyboardL, GamepadInput.GamePad.Index.One);        }        Vector2 shootAxis = GamepadInput.GamePad.GetAxis(GamepadInput.GamePad.Axis.RightStick, GamepadInput.GamePad.Index.One);        if (shootAxis == Vector2.zero)        {            shootAxis = GamepadInput.GamePad.GetAxis(GamepadInput.GamePad.Axis.KeyboardR, GamepadInput.GamePad.Index.One);        }
-        timeToNextShoot -= Time.fixedDeltaTime;        if (timeToNextShoot < 0.0f && shootAxis != Vector2.zero)        {            timeToNextShoot = shootRate;            GameObject shootAux = shootsPool.getObject(false);
+        if (timeBlocked > 0.0f) return;        movAxis = GamepadInput.GamePad.GetAxis(GamepadInput.GamePad.Axis.LeftStick, GamepadInput.GamePad.Index.One);        if (movAxis == Vector2.zero)        {            movAxis = GamepadInput.GamePad.GetAxis(GamepadInput.GamePad.Axis.KeyboardL, GamepadInput.GamePad.Index.One);        }        Vector2 shootAxis = GamepadInput.GamePad.GetAxis(GamepadInput.GamePad.Axis.RightStick, GamepadInput.GamePad.Index.One);        if (shootAxis == Vector2.zero)        {            shootAxis = GamepadInput.GamePad.GetAxis(GamepadInput.GamePad.Axis.KeyboardR, GamepadInput.GamePad.Index.One);        }        if (movAxis != Vector2.zero)        {            characterController.Move(new Vector3(movAxis.x, 0.0f, movAxis.y) * Time.fixedDeltaTime * movSpeed);        }        Vector3 lookDir;        if (shootAxis == Vector2.zero)        {            lookDir = (Vector3.right * movAxis.x + Vector3.forward * movAxis.y);        }        else        {            lookDir = (Vector3.right * shootAxis.x + Vector3.forward * shootAxis.y);        }        if (lookDir != Vector3.zero)        {
+            shootSpawn.rotation = Quaternion.LookRotation(lookDir, Vector3.up);            //myTransform.rotation = Quaternion.LookRotation(lookDir, Vector3.up);        }
+
+        timeToNextShoot -= Time.fixedDeltaTime;
+        if (timeToNextShoot < 0.0f && shootAxis != Vector2.zero)
+        {
+            timeToNextShoot = shootRate;
+            GameObject shootAux = shootsPool.getObject(false);
             Vector3 dir = (Vector3.right * shootAxis.x + Vector3.forward * shootAxis.y);
-            shootAux.GetComponent<Shoot>().Spawn(shootSpawn.position, shootSpawn.rotation, shootDamage, this.gameObject);        }        if (movAxis != Vector2.zero)        {            characterController.Move(new Vector3(movAxis.x, 0.0f, movAxis.y) * Time.fixedDeltaTime * movSpeed);        }        Vector3 lookDir;        if (shootAxis == Vector2.zero)        {            lookDir = (Vector3.right * movAxis.x + Vector3.forward * movAxis.y);        }        else        {            lookDir = (Vector3.right * shootAxis.x + Vector3.forward * shootAxis.y);        }        if (lookDir != Vector3.zero)        {            //myTransform.rotation = Quaternion.LookRotation(lookDir, Vector3.up);        }    }
+            shootAux.GetComponent<Shoot>().Spawn(shootSpawn.position, shootSpawn.rotation, shootDamage, this.gameObject);
+        }    }
     public void pickItem(Collectable.COLLECTABLES type)
     {
         collectable.Add(type);
