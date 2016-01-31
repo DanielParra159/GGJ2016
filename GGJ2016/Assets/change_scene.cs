ed3087dfb42a53a3f4093a237c5dc863f7e2a402
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2016-01-31 14:51:20 +0100

    menu

---
+++ GGJ2016/Assets/change_scene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class change_scene : MonoBehaviour {
+
+
+
+    public void LoadScene(int level)
+    {
+      
+        Application.LoadLevel(level);
+    }
+    public void exitGame() {
+        Application.Quit();
+    }
+
+}
---
+++ GGJ2016/Assets/flowerPower.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flowerPower : MonoBehaviour {
+
+
+    public GameObject door;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(door);
+
+        }
+    }
+}
