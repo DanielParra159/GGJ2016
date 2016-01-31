411fb0d3fb6d73bae4b0c91e2e8a8b147b3c2bd9
Daniel Parra <dani.parra.90@gmail.com>
2016-01-31 15:16:58 +0100

    + ultimos retoques

---
+++ GGJ2016/Assets/Scripts/Utils/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnEnable()
+    {
+        Application.LoadLevel(4);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
